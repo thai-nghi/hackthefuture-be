9051343ec93d3a4d0abfa0d8ddd8b3e9309c9dcb
thai-nghi <pthai.nghi@gmail.com>
2024-10-22 02:43:06 -0700

    integrate event list/detail

--- src/endpoints/event.py
+++ src/endpoints/event.py
@@ -11,19 +11,31 @@ from src import schemas
 from src.schemas import responses
 
 from src.services import event, organization
+import datetime
 
+router = APIRouter(prefix="/event", tags=["Events"])
 
-router = APIRouter(prefix="/event", tags=['Events'])
 
 @router.get("/", response_model=responses.EventListResponse)
 async def get_event(
     current_user: UserDependency,
     db_session: DatabaseDependency,
-    page_config: Annotated[schemas.PaginationIn, Query()]
+    currentPage: Annotated[int, Query(gt=0)] = 0,
+    pageSize: Annotated[int, Query(lt=100)] = 6,
+    country: Annotated[int | None, Query()] = None,
+    city: Annotated[int | None, Query()] = None,
+    tags: Annotated[list[int] | None, Query()] = None,
 ) -> responses.EventListResponse:
 
-    total, events = await event.get_all_event(db_session, page_config)
-    page_response = schemas.PaginationResponse(total=total, **page_config.dict())
+    total, events = await event.get_all_event(
+        db_session,
+        schemas.PaginationIn(currentPage=currentPage, pageSize=pageSize),
+        country=country,
+        city=city,
+        tags=tags,
+        start_after=datetime.datetime.now(),
+    )
+    page_response = schemas.PaginationResponse(total=total, currentPage=currentPage, pageSize=pageSize)
     return responses.EventListResponse(data=events, page=page_response)
 
 
@@ -32,40 +44,42 @@ async def create_event(
     current_user: UserDependency,
     db_session: DatabaseDependency,
     data: Annotated[schemas.EventAttributeIn, Body(embed=True)],
-) -> responses.EventAttributeResponse :
-    
+) -> responses.EventAttributeResponse:
+
     if current_user.organization_id is None:
         raise BadRequestException("You must be in an organization to create an event")
-    event_data = schemas.EventAttributeMid(organizer_id=current_user.organization_id, **data.dict())
+    event_data = schemas.EventAttributeMid(
+        organizer_id=current_user.organization_id, **data.dict()
+    )
     new_event = await event.create_new_event(db_session, event_data)
     await db_session.commit()
     return responses.EventAttributeResponse(data=new_event)
+
 
 @router.get("/{event_id}", response_model=responses.EventAttributeResponse)
 async def get_event(
     current_user: UserDependency,
     db_session: DatabaseDependency,
-    event_id: Annotated[int, Path]
+    event_id: Annotated[int, Path],
 ) -> responses.EventAttributeResponse:
-    
-    retrieved_event = (await event.get_event_by_id(db_session, event_id))
-    return responses.EventAttributeResponse(data = retrieved_event)
+
+    retrieved_event = await event.get_event_by_id(db_session, event_id)
+    return responses.EventAttributeResponse(data=retrieved_event)
+
 
 @router.put("/{event_id}", response_model=responses.EventAttributeResponse)
 async def get_event(
     current_user: UserDependency,
     db_session: DatabaseDependency,
     event_id: Annotated[int, Path],
-    event_data: Annotated[schemas.EventAttributeIn, Body(embed=True)]
+    event_data: Annotated[schemas.EventAttributeIn, Body(embed=True)],
 ) -> responses.EventAttributeResponse:
-    
+
     event_organizer_id = await event.get_organizer_by_id(db_session, event_id)
-    
-   
+
     if current_user.organization_id != event_organizer_id:
         raise BadRequestException("User are not the organizer of the event")
 
-    retrieved_event = (await event.update_event(db_session, event_id, event_data))
+    retrieved_event = await event.update_event(db_session, event_id, event_data)
     await db_session.commit()
-    return responses.EventAttributeResponse(data = retrieved_event)
-
+    return responses.EventAttributeResponse(data=retrieved_event)
